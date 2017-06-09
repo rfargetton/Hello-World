b6a36f3239caf8a2a9a9e8960f9b28715a72ff5a
rfargetton <romaric.fargetton@gmail.com>
2017-06-09 19:15:04 +0200

    added some more solutions

--- main.js
+++ main.js
@@ -1,12 +1,61 @@
 const str = 'Hello World!';
 document.querySelector('h1').innerHTML = str +" "+ str.length ;
 
+// Truncate a String
 
-var fruits = ["Banane", "Orange", "Citron", "Pomme", "Mangue"];
-var newArr = [];
-for (var i = 0; i < fruits.length; i += 2){
-  newArr.push(fruits.slice(i, i+2));
+function truncateString(str, num) {
+  if (str.length > num){
+    if (num <= 3) {
+      return str.slice(0, num)+"...";
+    } else {
+      return str.slice(0, num-3)+"...";
+    }
+  } else {
+    return str;
+  }
+
 }
 
-console.log(fruits.length/2);
-console.log(newArr);
+truncateString("A-tisket a-tasket A green and yellow basket", 11);
+
+// Chunky Monkey
+
+function chunkArrayInGroups(arr, size) {
+  var newArr = [];
+  for (var i = 0; i < arr.length; i += size){
+    newArr.push(arr.slice(i, i+size));
+  }
+return newArr;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
+
+// Slasher Flick
+
+function slasher(arr, howMany) {
+  arr.splice(0, howMany);
+  return arr;
+}
+
+slasher([1, 2, 3], 2);
+
+// Mutations
+
+function mutation(arr) {
+  var result ;
+  var firstArr = arr[0].toLowerCase();
+  var secondArr = arr[1].toLowerCase();
+  for(var i = 0; i<secondArr.length; i++){
+    if (firstArr.indexOf(secondArr[i]) == -1){
+      result = false ;
+      break;
+    } else {
+      result = true;
+    }
+  }
+  return result;
+}
+
+mutation(["Mary", "Aarmy"]);
+
+// Falsy Bouncer
