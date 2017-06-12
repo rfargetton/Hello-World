5269aa593694a3e81f3985ded0932e869cdbddf8
rfargetton <romaric.fargetton@gmail.com>
2017-06-12 13:51:21 +0200

    completed algorithm Section

--- main.js
+++ main.js
@@ -1,6 +1,93 @@
 const str = 'Hello FreeCodeCamp !';
 document.querySelector('h1').innerHTML = str ;
 
+// Reverse a String
+
+function reverseString(str) {
+  var reverse ="";
+  for(var i = str.length-1; i >= 0; i--){
+    reverse += str[i]  ;
+  }
+  return reverse;
+}
+
+reverseString("hello");
+
+// Factorialize a Number
+
+function factorialize(num) {
+  if (num === 0 || num === 1)
+    return 1;
+  for(var i=num-1; i>=1; i--){
+    num *= i ;
+  }
+  return num;
+}
+
+factorialize(5);
+
+// Check for Palindromes
+
+function palindrome(str) {
+  var lowerStr = str.toLowerCase().replace(/[^0-9a-z]/g, '');
+  return lowerStr == lowerStr.split("").reverse().join("");
+}
+
+palindrome("nope");
+
+// Find the Longest Word in a String
+
+function findLongestWord(str) {
+  str = str.split(' ');
+  var longestString = 0;
+  str.forEach(function(string){
+    if(string.length > longestString){
+      longestString = string.length;
+    }
+  });
+  return longestString;
+}
+
+findLongestWord("The quick brown fox jumped over the lazy dog");
+
+// Title Case a Sentence
+
+function titleCase(str) {
+  var newStr = str.toLowerCase().split(' ');
+  newStr.forEach(function(string, index){
+    newStr[index] = string.charAt(0).toUpperCase() + string.slice(1);
+  });
+  return newStr.join(' ');
+}
+
+titleCase("I'm a little tea pot");
+
+// Return Largest Numbers in Arrays
+
+function largestOfFour(arr) {
+  var biggestArray = [];
+  arr.forEach(function(thisArray){
+    var biggest = 0;
+    thisArray.forEach(function(thisNumber){
+      if (thisNumber>biggest){
+        biggest = thisNumber;
+      }
+    });
+    biggestArray.push(biggest);
+  });
+  return biggestArray;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+// Confirm Ending
+
+function confirmEnding(str, target) {
+  return target == str.substring(str.length - target.length);
+}
+
+confirmEnding("Bastian", "n");
+
 // Truncate a String
 
 function truncateString(str, num) {
@@ -59,3 +146,53 @@ function mutation(arr) {
 mutation(["Mary", "Aarmy"]);
 
 // Falsy Bouncer
+
+function bouncer(arr) {
+  return arr.filter(Boolean);
+}
+
+bouncer([7, "ate", "", false, 9]);
+
+// Seek and Destroy
+
+function destroyer(arr) {
+  var args = Array.from(arguments);
+  var newArr = args.splice(1);
+  return arr.filter(function(value){
+    return newArr.indexOf(value) === -1;
+  });
+}
+
+destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3);
+
+// Where do i belong
+
+function getIndexToIns(arr, num) {
+  arr.push(num);
+  arr.sort(function(a, b){
+    return a - b ;
+  });
+  num = arr.indexOf(num);
+  return num;
+}
+
+getIndexToIns([2, 20, 10], 19);
+
+// Caesars Cipher
+
+function rot13(str) {
+  var newStr =  '';
+  for (var i = 0; i < str.length; i++){
+    var charCode = str.charCodeAt(i);
+    if (charCode < 65 || charCode > 90){
+      newStr += String.fromCharCode(charCode) ;
+    } else if (charCode < 78) {
+      newStr += String.fromCharCode(charCode + 13) ;
+    } else {
+      newStr += String.fromCharCode(charCode - 13) ;
+    }
+  }
+  return newStr;
+}
+
+console.log(rot13("SERR PBQR PNZC"));
